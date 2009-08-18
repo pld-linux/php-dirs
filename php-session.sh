34676030f540519e49c77b25a2a954bc21f6dc17
Arkadiusz Miśkiewicz <arekm@maven.pl>
2009-08-18 06:19:34 +0000

    - better matching and rounding. will switch to minutes if https://fedorahosted.org/tmpwatch/ticket/7 is implemented
    Changed files:
        php-session.sh -> 1.2

--- php-session.sh
+++ php-session.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 if [ -x /usr/bin/php.cli ] ; then
-	RUNTIME5=$(/usr/bin/php.cli -r 'echo 1+(int)(ini_get("session.gc_maxlifetime")/3600);' 2> /dev/null)
+	RUNTIME5=$(/usr/bin/php.cli -r '$t = round(ini_get("session.gc_maxlifetime")/3600); if ($t<1) $t=1; echo $t;' 2> /dev/null)
 elif [ -r /etc/php/php.ini ]; then
-	RUNTIME5=$(awk -F"=" '/^session.gc_maxlifetime[[:space:]]*=/ { t=sprintf("%d", 1+($2/3600)); print t;}' /etc/php/php.ini)
+	RUNTIME5=$(awk -F"=" '/^session.gc_maxlifetime[ \t]*=/ { t=sprintf("%d", ($2/3600)); if (t<1) { t=1; }; print t;}' /etc/php/php.ini)
 fi
 
 if [ -x /usr/bin/php4.cli ] ; then
-	RUNTIME4=$(/usr/bin/php4.cli -r 'echo 1+(int)(ini_get("session.gc_maxlifetime")/3600);' 2> /dev/null)
+	RUNTIME4=$(/usr/bin/php4.cli -r '$t = round(ini_get("session.gc_maxlifetime")/3600); if ($t<1) $t=1; echo $t;' 2> /dev/null)
 elif [ -r /etc/php4/php.ini ]; then
-	RUNTIME4=$(awk -F"=" '/^session.gc_maxlifetime[[:space:]]*=/ { t=sprintf("%d", 1+($2/3600)); print t;}' /etc/php4/php.ini)
+	RUNTIME4=$(awk -F"=" '/^session.gc_maxlifetime[ \t]*=/ { t=sprintf("%d", ($2/3600)); if (t<1) { t=1; }; print t;}' /etc/php4/php.ini)
 fi
 
 [ -z "$RUNTIME5" ] && RUNTIME5="1h"
