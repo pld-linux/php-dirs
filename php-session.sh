a77f978620dbd9c457b98c99e6daedfe664a7967
Elan Ruusamäe <glen@pld-linux.org>
2011-06-21 11:30:09 +0000

    - simplify
    Changed files:
        php-session.sh -> 1.7

--- php-session.sh
+++ php-session.sh
@@ -3,13 +3,13 @@
 [ -x /usr/sbin/tmpwatch ] || exit
 
 if [ -x /usr/bin/php ]; then
-	RUNTIME5=$(/usr/bin/php -r '$t = round(ini_get("session.gc_maxlifetime")/3600); if ($t<1) $t=1; echo $t;' 2> /dev/null)
+	RUNTIME5=$(/usr/bin/php -r 'echo max(round(ini_get("session.gc_maxlifetime")/3600), 1);' 2> /dev/null)
 elif [ -r /etc/php/php.ini ]; then
 	RUNTIME5=$(awk -F"=" '/^session.gc_maxlifetime[ \t]*=/ { t=sprintf("%d", ($2/3600)); if (t<1) { t=1; }; print t; exit;}' /etc/php/php.ini)
 fi
 
 if [ -x /usr/bin/php4 ]; then
-	RUNTIME4=$(/usr/bin/php4 -r '$t = round(ini_get("session.gc_maxlifetime")/3600); if ($t<1) $t=1; echo $t;' 2> /dev/null)
+	RUNTIME4=$(/usr/bin/php4 -r 'echo max(round(ini_get("session.gc_maxlifetime")/3600), 1);' 2> /dev/null)
 elif [ -r /etc/php4/php.ini ]; then
 	RUNTIME4=$(awk -F"=" '/^session.gc_maxlifetime[ \t]*=/ { t=sprintf("%d", ($2/3600)); if (t<1) { t=1; }; print t; exit;}' /etc/php4/php.ini)
 fi
